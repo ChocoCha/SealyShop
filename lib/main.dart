2b5936fbfecfedbccdd4cb34a14f92b05c3bec95
ChocoCha <plangtam2@gmail.com>
2025-10-31 12:18:16 +0700

    3:25:59 ทำหน้าแอดโปรดัค ยังไม่เชื่อมแบ็ค

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:sealyshop/Admin/add_product.dart';
 import 'package:sealyshop/Admin/admin_login.dart';
 import 'package:sealyshop/pages/signup.dart';
 
@@ -38,7 +39,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: AdminLogin(),
+      home: AddProduct(),
     );
   }
 }
